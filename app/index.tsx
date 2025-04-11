acfed2fd3c7729b8a4856dfc5c05b982cac379f1
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-04-11 15:24:48 +0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import "@/global.css";
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+export default function HomeScreen() {
+  return (
+    <>
+      <SafeAreaView>
+        <View className="">
+          <Text> Hello</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
