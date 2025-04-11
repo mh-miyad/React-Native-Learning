eab5d8cff0790c01817913bb565e50b0d8b64442
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-04-11 16:53:16 +0600

    loader screen is done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,8 @@
 import "@/global.css";
 import { Slot } from "expo-router";
 import React from "react";
-import { SafeAreaView } from "react-native";
 function Layout() {
-  return (
-    <SafeAreaView className="flex-1 bg-gray-900">
-      <Slot />
-    </SafeAreaView>
-  );
+  return <Slot />;
 }
 
 export default Layout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,18 @@
-import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 function HomeScreen() {
   return (
     <>
-      <View className="pt-5">
-        <Text className="uppercase text-red-400 text-center text-5xl ">
-          This sis Index pages
-        </Text>
-        <Link
-          href="/home/home"
-          className="cursor-pointer my-10 py-4 bg-white border w-1/2 mx-auto rounded-full group"
-        >
-          <Text className="uppercase text-red-400 text-center text-2xl group-hover:text-black">
-            hello world
-          </Text>
-        </Link>
-      </View>
+      <ImageBackground
+        src="https://images.unsplash.com/photo-1698993001180-8654ab29032a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        className="flex-1 h-full w-full "
+        resizeMode="cover"
+      >
+        <View className={"absolute inset-0 bg-black/50"} />
+        <View className=" h-full flex-1  justify-center items-center">
+          <Text className="text-white text-5xl pt-[400px]">Home Screen</Text>
+        </View>
+      </ImageBackground>
     </>
   );
 }
